0bc5a93f4fdce5ee26614ce08314e7ce5aca31c0
maajidpp <majimaajidpp@gmail.com>
2021-10-21 16:22:05 +0530

    my_first_commit

---
+++ classobject.py
@@ -0,0 +1,17 @@
+class actions:
+    def smile(self):
+        print("HA ha")
+    def tired(self):
+        print("phew")
+ac = actions()
+ac.smile()
+ac.tired()"""
+
+"""class animals:
+    def dog(self):
+        print("bow bow")
+    def cat(self):
+        print("meow meow")
+sound = animals()
+sound.dog()
+sound.cat()
